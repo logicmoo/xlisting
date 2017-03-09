d688daa513a917b31ae7ee6f06df1286cab76583
root <logicmoo@gmail.com>
2017-03-09 15:05:21 -0800

    v1.1.112

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Provides Cross Referenced Listing and Source Variables at Console
 
 Installation using SWI-Prolog 7.1 or later:
 
-    `?- pack_install('https://github.com/logicmoo/xlisting.git'). `
+    `?- pack_install('https://github.com/TeamSPoon/xlisting.git'). `
 
 Source code available and pull requests accepted at
 http://github.com/logicmoo/xlisting
--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(xlisting).
 title('Selective Interactive Non-Deterministic Tracing').
-version('1.1.111').
-download('https://github.com/logicmoo/xlisting/releases/*.zip').
+version('1.1.112').
+download('https://github.com/TeamSPoon/xlisting/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
-home('https://github.com/logicmoo/xlisting').
+home('https://github.com/TeamSPoon/xlisting').
 requires(clause_attvars).
 autoload(true).
