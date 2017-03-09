f433497c382c27b2ab5a11d7c881a3baaa656f3c
root <logicmoo@gmail.com>
2017-03-08 17:32:37 -0800

    v1.1.111

--- pack.pl
+++ pack.pl
@@ -1,11 +1,10 @@
 name(xlisting).
 title('Selective Interactive Non-Deterministic Tracing').
-version('0.0.8').
+version('1.1.111').
 download('https://github.com/logicmoo/xlisting/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home('https://github.com/logicmoo/xlisting').
-requires(logicmoo_utils).
 requires(clause_attvars).
 autoload(true).
